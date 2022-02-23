2d6e14f639bf88c1be63ebedde16677a121de2e8
krsgarland <97922634+krsgarland@users.noreply.github.com>
2022-02-23 13:59:46 -0600

    Class 3 Exercises

---
+++ Exercise 1/main.js
@@ -0,0 +1,11 @@
+//Create a function
+//Have that function take place in an argument
+//Print a statement that uses the variable with some other test
+
+function startHomework (time){
+    console.log(`Start your homework at: ${time}`);
+}
+
+startHomework ("4pm");
+
+
---
+++ Exercise 2/main.js
@@ -0,0 +1,10 @@
+//Create the HTML with a button
+//Create a function that takes in a user object
+//Print each item in the user object out in a string to the console
+
+function printUserInfo(user){
+    console.log(user.username);
+    console.log(user.password);
+    console.log(user.isAdmin);
+}
+
